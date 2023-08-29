db22e93bf3d524a722b998470ac92d8ebc4f58c3
zzimnii <happykeil@naver.com>
2023-08-29 21:00:52 +0900

    feat: User custom  Profile Image

--- travellion/accounts/urls.py
+++ travellion/accounts/urls.py
@@ -2,14 +2,19 @@ from django.urls import path, include
 from .views import UserCreate, LoginView, ProfileList
 from rest_framework_simplejwt import views as jwt_views
 from .views import BlacklistRefreshView
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('signup/', UserCreate.as_view()),
     path('login/', LoginView.as_view()),
     
-    path('profile/<int:pk>/', ProfileList.as_view()), 
+    path('profile/<uuid:userId>/', ProfileList.as_view()), 
 
     path('', include('rest_framework.urls')),
     path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
     path('logout/', BlacklistRefreshView.as_view(), name="logout"), 
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- travellion/travellion/settings.py
+++ travellion/travellion/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -111,7 +112,7 @@ SIMPLE_JWT = {
 
     'AUTH_HEADER_TYPES': ('Bearer',),
     'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
-    'USER_ID_FIELD': 'id',
+    'USER_ID_FIELD': 'userId',
     'USER_ID_CLAIM': 'user_id',
     'USER_AUTHENTICATION_RULE': 'rest_framework_simplejwt.authentication.default_user_authentication_rule',
 
@@ -204,6 +205,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
