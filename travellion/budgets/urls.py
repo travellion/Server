fae2921b9780e143bb66edd6c016ef53cacdf439
da-jeong <loveda0529@naver.com>
2024-01-09 23:41:12 +0900

    fix:그룹초대 수정

--- travellion/budgets/urls.py
+++ travellion/budgets/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include, re_path
-from .views import GroupViewSet, PlanViewSet, CategoryViewSet, GroupListSet, ExchangeViewSet, InviteMemberAPIView, JoinGroupAPIView, InviteMembersAPIView
+from .views import GroupViewSet, PlanViewSet, CategoryViewSet, GroupListSet, ExchangeViewSet, JoinGroupAPIView, InviteMembersAPIView
 from rest_framework.routers import SimpleRouter, DefaultRouter
 
 group_router = SimpleRouter(trailing_slash=False)
@@ -27,6 +27,6 @@ urlpatterns = [
     path('', include(api_router.urls)),
 
     # path('invite/', InviteMemberAPIView.as_view(), name='invite_member'),
-    path('join/<int:group_id>/', JoinGroupAPIView.as_view(), name='join_group'),
+    path('join/', JoinGroupAPIView.as_view(), name='join_group'),
     path('groups/<int:group_id>/invite/', InviteMembersAPIView.as_view(), name='invite_member'),
 ]
